1643a6b0f6587c5327df1c42f47fd8c3d4636009
may arden <mayard3n@gmail.com>
2021-10-24 12:06:45 -0700

    builds navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,29 @@
-// navigation menu goes here 
+import React from 'react' 
+import { Navbar } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import { NavDropDown } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class NavBar extends React.Component {
+    render() {
+        return (
+
+            <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="/">composer-db</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" >
+                <Nav className="mr-auto">
+                    <Nav.link href="/composers/new">composers submission</Nav.link>
+                    <NavDropdown title="categories" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/categories">all categories</NavDropdown.Item>
+                        <NavDropdown.Divier /> 
+                        <CatDropdown categories={this.props.categories} />
+                    </NavDropdown>
+                    </Nav>
+            </Navbar.Collapse> 
+            </Navbar> 
+        )
+    }
+}
+
+export default NavBar;
