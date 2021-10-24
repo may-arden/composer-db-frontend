d60f004a54c44363f9d785da6a6028d68ae59b56
may arden <mayard3n@gmail.com>
2021-10-24 15:46:47 -0700

    builds  POST composer reducer

--- src/reducers/composersReducer.js
+++ src/reducers/composersReducer.js
@@ -4,6 +4,8 @@ export const composersReducer = (state = [], action) => {
     switch(action.type) {
         case 'FETCH_COMPOSERS':
             return action.payload 
+        case 'ADD_COMPOSER':
+            return [...state, action.payload]
         default: return state 
     }
 }
